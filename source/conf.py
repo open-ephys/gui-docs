3b2f8f2525f1a0a0fbfb11a0fe06e8dfd2b7a964
Josh Siegle <joshs@alleninstitute.org>
2025-12-22 13:30:17 -0800

    Enable sphinx_design extension in conf.py

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,8 @@ release = "0.0.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.todo", 
-              "sphinx.ext.githubpages"]
+              "sphinx.ext.githubpages",
+              "sphinx_design"]
 
 
 # Add any paths that contain templates here, relative to this directory.
